8e6ad683be78df11da48ebf088f968261b445bef
sushilkundu143 <sushilkundu143@gmail.com>
2024-12-08 19:21:33 +0530

    update the app

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import AppRoutes from "./router/Routes";
 
 function App() {
+  // deplying the avoma app
   return (
     <div className="container" data-testid="app">
       <AppRoutes />
